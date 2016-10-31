41b96de3bbcce642b84270b8f51ff58e54619bce
Brian Dutz and Jon Toler <brian+jon@>
2016-10-31 14:19:00 -0700

    adding in categories model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,22 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('listing');
-  },
-  actions: {
-    saveListing(params) {
-      var newListing = this.store.createRecord('listing', params);
-      newListing.save();
-      this.transitioTo('index');
+  export default Ember.Route.extend({
+    model() {
+      return Ember.RSVP.hash({
+        listings: this.store.findAll('listing'),
+        categories: this.store.findAll('category')
+      });
+    },
+    actions: {
+      saveListing(params) {
+        var newListing = this.store.createRecord('listing', params);
+        newListing.save();
+        this.transitionTo('index');
+    },
+    saveCategory(params) {
+      var newCategory = this.store.createRecord('category', params);
+      newCategory.save();
+      this.transitionTo('index');
     }
   }
 });
